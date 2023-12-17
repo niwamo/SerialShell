74552de51e31c43d241244a2b420b8b960986b97
niwamo <nm@niwamo.com>
2023-12-16 20:24:03 -0600

    remove/comment out private paths in README

--- PSSerial.psm1
+++ PSSerial.psm1
@@ -5,8 +5,8 @@ $publicFunctionsPath = `
     $PSScriptRoot + $directorySeparator + 'Public'
 
 # Private Dir
-$privateFunctionsPath = `
-    $PSScriptRoot + $directorySeparator + 'Private'
+#$privateFunctionsPath = `
+#    $PSScriptRoot + $directorySeparator + 'Private'
 
 # Classes Dir
 $classesPath =  $PSScriptRoot + $directorySeparator + 'Classes'
@@ -14,15 +14,15 @@ $classesPath =  $PSScriptRoot + $directorySeparator + 'Classes'
 # Source files
 $publicFunctions = Get-ChildItem -Path $publicFunctionsPath | `
     Where-Object {$_.Extension -eq '.ps1'}
-$privateFunctions = Get-ChildItem -Path $privateFunctionsPath | `
-    Where-Object {$_.Extension -eq '.ps1'}
+#$privateFunctions = Get-ChildItem -Path $privateFunctionsPath | `
+#    Where-Object {$_.Extension -eq '.ps1'}
 $classes = Get-ChildItem -Path $classesPath | `
     Where-Object {$_.Extension -eq '.ps1'}
 $publicFunctions | ForEach-Object { . $_.FullName }
-$privateFunctions | ForEach-Object { . $_.FullName }
+#$privateFunctions | ForEach-Object { . $_.FullName }
 $classes | ForEach-Object { . $_.FullName }
 
 # Export all of the public functions from this module
 foreach ($func in $publicFunctions) { 
-    Export-ModuleMember -Function $_.BaseName
+    Export-ModuleMember -Function $func.BaseName
 }
